b0785e46006c98b3b0be31b7cb310105fadea7a0
Koray Dundar <koraydundar_40@hotmail.com>
2019-09-27 10:56:09 +0300

    fixed button links for

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -7,7 +7,7 @@ const Project = ({ image, title, link }) => {
   return (
     <figure className="portfolio__photos">
       <div className="inside">
-        <a className="inside__link" href="#">
+        <a className="inside__link" href={link} target="_blank" rel="noopener noreferrer">
           <Img className="inside__pic" fixed={image} alt={title} />
           <span className="portfolio__hidden-title">
             <a href={link} target="_blank" rel="noopener noreferrer">
